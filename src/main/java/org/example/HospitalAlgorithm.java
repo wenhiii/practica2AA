993c05106932c61bb06c354f79d584f9863293ea
wenhiii <wenhao20040426zhang@gmail.com>
2025-10-11 20:21:40 +0200

    Final Version

--- src/main/java/org/example/HospitalAlgorithm.java
+++ src/main/java/org/example/HospitalAlgorithm.java
@@ -5,21 +5,25 @@ public class HospitalAlgorithm {
     /**
      * Optimal selection using DP: choose hospitals to maximize total population
      * served, ensuring any two chosen hospitals are >5 units apart.
+     *
+     * @param xs positions of the hospitals
+     * @param ps population served by each hospital
+     * @return total population served by the selected hospitals
      */
-    public static int calculateOptimalDP(int[] positions, int[] populations) {
-        int n = positions.length;
+    public static int calculateOptimalDP(int[] xs, int[] ps) {
+        int n = xs.length;
         int[] dp = new int[n];
 
-        dp[0] = populations[0];
+        dp[0] = ps[0];
 
         for (int i = 1; i < n; i++) {
             // Find the largest j < i such that positions[j] < positions[i] - 5
             int lo = 0, hi = i - 1, prev = -1;
-            int target = positions[i] - 5; // Minimum position that a previous hospital can have (at least 5 units
+            int target = xs[i] - 5; // Minimum position that a previous hospital can have (at least 5 units
             // apart)
             while (lo <= hi) {
-                int mid = (lo + hi) >>> 1;
-                if (positions[mid] < target) {
+                int mid = (lo + hi) / 2;
+                if (xs[mid] < target) {
                     prev = mid;
                     lo = mid + 1;
                 } else {
@@ -29,9 +33,9 @@ public class HospitalAlgorithm {
 
             int withCurrent;
             if (prev >= 0) {
-                withCurrent = populations[i] + dp[prev];
+                withCurrent = ps[i] + dp[prev];
             } else {
-                withCurrent = populations[i];
+                withCurrent = ps[i];
             }
 
             int withoutCurrent = dp[i - 1];
@@ -45,27 +49,109 @@ public class HospitalAlgorithm {
      * Greedy by distance: pick a hospital if it is strictly more than 5 units
      * away from the previously chosen hospital. Returns the total population
      * served.
+     *
+     * @param xs positions of the hospitals
+     * @param ps population served by each hospital
+     * @return total population served by the selected hospitals
      */
-    public static int calculateGreedyDistance(int[] positions, int[] populations) {
+    public static int calculateGreedyDistance(int[] xs, int[] ps) {
         int total = 0;
 
-        int n = positions.length;
-        int lastChosen = -1; // ensures first candidate can be chosen if appropriate
+        int n = xs.length;
+        int lastChosen = -1;
 
         for (int i = 0; i < n; i++) {
-            // Select the hospital if it is far enough from the last chosen one
-            if (positions[i] - lastChosen > 5) {
-                total += populations[i]; // Add the number of people served by this hospital
-                lastChosen = positions[i]; // Update the last chosen hospital position
+            if (xs[i] - lastChosen > 5) {
+                total += ps[i];
+                lastChosen = xs[i];
             }
         }
 
         return total;
     }
 
-    public static int calculateGreedyValue(int[] positions, int[] populations) {
-        // Not implemented yet
-        return 0;
+    public static int calculateGreedyFromMax(int[] xs, int[] ps) {
+        int n = xs.length;
+
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (ps[i] > ps[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        int total = ps[maxIndex];
+        int lastChosen = xs[maxIndex];
+
+        for (int i = maxIndex + 1; i < n; i++) {
+            if (xs[i] - lastChosen > 5) {
+                total += ps[i];
+                lastChosen = xs[i];
+            }
+        }
+
+        lastChosen = xs[maxIndex];
+        for (int i = maxIndex - 1; i >= 0; i--) {
+            if (lastChosen - xs[i] > 5) {
+                total += ps[i];
+                lastChosen = xs[i];
+            }
+        }
+
+        return total;
+    }
+
+    /**
+     * Heuristic selection by population: Picks hospitals in order of highest
+     * population, adding them if they are >5 units apart from already chosen
+     * hospitals. Returns total population served.
+     *
+     * @param xs positions of the hospitals
+     * @param ps population served by each hospital
+     * @return total population served by the selected hospitals
+     */
+    public static int sortIndicesByValueDescending(int[] xs, int[] ps) {
+        int[] sortedIndices = sortIndices(ps);
+        boolean[] xsOccupied = new boolean[xs.length];
+        int maxCapacity = ps[sortedIndices[0]];
+        xsOccupied[sortedIndices[0]] = true;
+        int index;
+
+        for (int i = 0; i < xs.length; i++) {
+            index = sortedIndices[i];
+            if (!xsOccupied[index]) {
+                int index2;
+                boolean validCandidate = true;
+                for (int j = 0; j < xs.length && validCandidate; j++) {
+                    index2 = sortedIndices[j];
+                    if (xsOccupied[index2]) {
+                        if ((xs[index] - xs[index2] <= 5) && (xs[index2] - xs[index] <= 5)) {
+                            validCandidate = false;
+                        }
+                    }
+                }
+                xsOccupied[index] = validCandidate;
+                if (validCandidate) {
+                    maxCapacity += ps[index];
+                }
+            }
+        }
+
+        return maxCapacity;
+    }
+
+    private static int[] sortIndices(int[] array) {
+        int[] sortedIndices = new int[array.length];
+        sortedIndices[0] = 0;
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j;
+            for (j = i - 1; j >= 0 && array[sortedIndices[j]] < value; j--) {
+                sortedIndices[j + 1] = sortedIndices[j];
+            }
+            sortedIndices[j + 1] = i;
+        }
+        return sortedIndices;
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] examplePositions = {
-                { 6, 7, 12, 14 },
-                { 0, 6, 12, 18 },
-                { 1, 3, 5, 7, 9 },
-                { 2, 4, 10, 11, 17 },
-                { 0, 2, 6, 9, 15, 16, 22 }
+            {6, 7, 12, 14},
+            {0, 6, 12, 18},
+            {1, 3, 5, 7, 9},
+            {2, 4, 10, 11, 17},
+            {0, 2, 6, 9, 15, 16, 22}
         };
 
         int[][] examplePopulations = {
-                { 5, 6, 5, 1 },
-                { 4, 5, 7, 4 },
-                { 2, 3, 4, 5, 6 },
-                { 4, 3, 7, 6, 5 },
-                { 1, 8, 5, 2, 9, 3, 4 }
+            {5, 6, 5, 1},
+            {4, 5, 7, 4},
+            {2, 3, 4, 5, 6},
+            {4, 3, 7, 6, 5},
+            {1, 8, 5, 2, 9, 3, 4}
         };
 
         for (int t = 0; t < examplePositions.length; t++) {
@@ -27,12 +27,16 @@ public class Main {
 
             int optimalTotal = HospitalAlgorithm.calculateOptimalDP(positions, populations);
             int greedyTotal = HospitalAlgorithm.calculateGreedyDistance(positions, populations);
+            int greedyTotal2 = HospitalAlgorithm.calculateGreedyFromMax(positions, populations);
+            int greedyTotal3 = HospitalAlgorithm.sortIndicesByValueDescending(positions, populations);
 
             System.out.println("=== Example " + (t + 1) + " ===");
             System.out.println("Positions: " + Arrays.toString(positions));
             System.out.println("Populations: " + Arrays.toString(populations));
             System.out.println(" -> Optimal total: " + optimalTotal);
-            System.out.println(" -> Greedy total:  " + greedyTotal);
+            System.out.println(" -> GreedyFromDistance total:  " + greedyTotal);
+            System.out.println(" -> GreedyFromMax total:  " + greedyTotal2);
+            System.out.println(" -> GreedyByHospitalValue total:  " + greedyTotal3);
             System.out.println("Selected hospitals (positions):");
             System.out.println("-----------------------------\n");
         }
